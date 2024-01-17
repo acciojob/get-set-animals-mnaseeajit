a8d428de7970df6a39c25039425c51d8c36d04bb
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-17 13:26:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,70 @@
-//complete this code
+// //complete this code
+// class Animal {
+// 	constructor(species){
+// 		this._species = species;
+// 	}
+// 	get species(){
+// 		return this._species;
+// 	}
+// 	makeSound(){
+// 		console.log("animal sound");
+// 	}
+// }
+
+// class Dog extends Animal {
+// 	constructor(species){
+// 		super(species);
+// 	}
+// 	bark(){
+// 		console.log("woof");
+// 	}
+// }
+
+// class Cat extends Animal {
+// 	constructor(species){
+// 		super(species);
+// 	}
+// 	purr(){
+// 		console.log("purr");
+//     }
+// }
+
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+
 class Animal {
-	constructor(species){
-		this._species = species;
-	}
-	get species(){
-		return this._species;
-	}
-	makeSound(){
-		console.log("animal sound");
-	}
+    constructor(species) {
+        this._species = species; // Use a different name for the property to avoid conflicts
+    }
+
+    get species() {
+        return this._species;
+    }
+
+    makeSound() {
+        console.log("animal sound");
+    }
 }
 
 class Dog extends Animal {
-	constructor(species){
-		super(species);
-	}
-	bark(){
-		console.log("woof");
-	}
+    constructor(species) {
+        super(species);
+    }
+
+    bark() {
+        console.log("woof");
+    }
 }
 
 class Cat extends Animal {
-	constructor(species){
-		super(species);
-	}
-	purr(){
-		console.log("purr");
+    constructor(species) {
+        super(species);
+    }
+
+    purr() {
+        console.log("purr");
     }
 }
 
@@ -33,3 +72,4 @@ class Cat extends Animal {
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
